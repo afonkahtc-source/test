110f992f50b971a37db57c603bb657e020da191a
Andrey <andreyrus105>
2025-07-29 14:27:52 +0300

    Добавлен setMusic в MusicPlayer и изменён bean musicPlayer

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,8 +6,6 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-//        Music music = context.getBean("musicBeanClassical",Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
         MusicPlayer musicPlayer = (MusicPlayer) context.getBean(
                 "musicPlayer");
