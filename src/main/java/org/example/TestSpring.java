20d90164638e10c70358a2dcc7ad13aaf6c2a6c7
Andrey <andreyrus105>
2025-07-29 14:03:55 +0300

    Configured IoC, changed TestSpring and applicationContext.xml

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,10 +6,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music music = context.getBean("musicBeanClassical",Music.class);
+//        Music music = context.getBean("musicBeanClassical",Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = (MusicPlayer) context.getBean(
+                "musicPlayer");
         musicPlayer.playMusic();
 
         context.close();
