d18ba6c4a817a9654dab948d01a859dac586fc17
Andrey <andreyrus105>
2025-08-01 11:45:48 +0300

    Выполнен урок 9. Созданы бины в java коде благодаря аннотациям

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component("musicBean")
+@Component
 public class RockMusic implements Music {
     @Override
     public String getSong() {
