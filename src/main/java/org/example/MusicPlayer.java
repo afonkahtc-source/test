9e944bfe226bbc4c338c5960b690316b6319cfb2
Andrey <andreyrus105>
2025-08-01 12:03:35 +0300

    Подготовка к 10 уроку. Отредактирован MusicPlayer

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,17 +1,10 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MusicPlayer {
     private Music music;
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+    public MusicPlayer(Music music) {this.music = music;}
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
+    public void playMusic() {System.out.println("Playing: " + music.getSong());}
 }
