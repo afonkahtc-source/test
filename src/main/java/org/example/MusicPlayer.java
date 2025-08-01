5e95e3b548ea59a7f44b311f7518933c93c9f644
Andrey <andreyrus105>
2025-08-01 11:39:23 +0300

    Подчищен MusicPlayer

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -4,38 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
-    private List<Music> musicList = new ArrayList<>();
+    private Music music;
 
-    private String name;
-    private int volume;
-
-    public List<Music> getMusicList() {
-        return musicList;
+    // IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
     }
 
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void  playMusic(){
-        for (Music music : musicList) {
-            System.out.println("Playing: " + music.getSong());
-        }
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
     }
 }
