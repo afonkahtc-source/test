062d2d64e4fa0e04f8840458c394ce688ec83373
Andrey <andreyrus105>
2025-08-01 12:09:14 +0300

    В MusicPlayer добавлены аннотации @Component и @Autowired. Исключена неопределённость

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
 
-    // IoC
-    public MusicPlayer(Music music) {this.music = music;}
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic) {this.classicalMusic = classicalMusic;}
 
-    public void playMusic() {System.out.println("Playing: " + music.getSong());}
+    public void playMusic() {System.out.println("Playing: " + classicalMusic.getSong());}
 }
